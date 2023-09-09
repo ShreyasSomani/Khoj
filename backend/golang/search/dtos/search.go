ec52f63aea25085ad56eb3a413873cb7b29330a6
Riyo <pradeepbhatm25@gmail.com>
2023-09-09 06:31:53 +0530

    search done

--- backend/golang/search/dtos/search.go
+++ backend/golang/search/dtos/search.go
@@ -1,17 +1,1 @@
 package dtos
-
-type Product struct {
-	ID          string  `json:"id"`
-	Name        string  `json:"name"`
-	SalePrice   float64 `json:"sale_price"`
-	MarketPrice float64 `json:"market_price"`
-	Type        string  `json:"type"`
-	Quantity    int     `json:"quantity"`
-	Category    string  `json:"category"`
-	SubCategory string  `json:"sub_category"`
-	Brand       string  `json:"brand"`
-	Rating      float64 `json:"rating"`
-	ImageUrl    string  `json:"image_url"`
-	ProductUrl  string  `json:"product_url"`
-	IsAvailable bool    `json:"is_available"`
-}
